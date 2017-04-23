97dce221e86abdbff21e43c0cac72ec4c45e8a39
Piotr Wiącek <pi.wiacek@gmail.com>
2017-04-23 22:49:16 +0200

    Charging areas in progress.

---
+++ LudumDare38/Assets/Scripts/GameObjects/SpecialArea.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialArea : MonoBehaviour {
+
+    public float BonusChargeValue = 0f;
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter", other.gameObject);
+        if (other.isTrigger)
+        {
+            Debug.Log("trigger");
+            return;
+        }
+
+        Debug.Log("not trigger");
+        Meeple meeple = other.GetComponentInParent<Meeple>();
+        if (meeple != null)
+        {
+            meeple.CurrentAreaBonus = BonusChargeValue;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("exit", other.gameObject);
+
+        if (other.isTrigger)
+        {
+            Debug.Log("trigger");
+            return;
+        }
+
+        Debug.Log("not trigger");
+        Meeple meeple = other.GetComponentInParent<Meeple>();
+        if (meeple != null)
+        {
+            // Assuming there are no areas overlapping.
+            meeple.CurrentAreaBonus = 0f;
+        }
+    }
+}
