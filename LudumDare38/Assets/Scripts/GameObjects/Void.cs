cc4a1c5ab56aad7c78835d950edf9b71f07f9356
Piotr Wiącek <pi.wiacek@gmail.com>
2017-04-24 18:27:18 +0200

    Some debug logs for debug purposes.

--- LudumDare38/Assets/Scripts/GameObjects/Void.cs
+++ LudumDare38/Assets/Scripts/GameObjects/Void.cs
@@ -10,8 +10,17 @@ public class Void : MonoBehaviour
 	private int spriteOrder = 0;
 
 	void OnTriggerEnter2D(Collider2D other)
-	{
-		if(GameplayManager.Instance.GameOver)
+    {
+        Debug.Log("enter", other.gameObject);
+        if (other.isTrigger)
+        {
+            Debug.Log("trigger");
+        }
+        else
+        {
+            Debug.Log("not trigger");
+        }
+        if (GameplayManager.Instance.GameOver)
 			return;
 
 		if(other.GetComponentInParent<Meeple>() != null
